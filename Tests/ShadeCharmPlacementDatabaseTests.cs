19b8f659f4bb858e9d8a0b6caa3a0cfdcc5491a7
CardiaDarkhill <killzonerdemon@gmail.com>
2025-09-25 06:22:37 +1000

    Add Fury and Compass world placements

--- Tests/ShadeCharmPlacementDatabaseTests.cs
+++ Tests/ShadeCharmPlacementDatabaseTests.cs
@@ -30,7 +30,7 @@ namespace LegacyoftheAbyss.Tests
             var placements = Shade.ShadeCharmPlacementDatabase.GetPlacementsForScene("MossGrotto");
 
             Assert.NotNull(placements);
-            Assert.Equal(11, placements.Count);
+            Assert.Equal(10, placements.Count);
 
             var kingsoul = placements.First(p => p.CharmId == Shade.ShadeCharmId.Kingsoul);
             Assert.NotNull(kingsoul.AnchorOffset);
@@ -52,6 +52,38 @@ namespace LegacyoftheAbyss.Tests
             Assert.Equal(74.772f, soulEater.WorldPosition!.X, 3);
             Assert.Equal(8.587f, soulEater.WorldPosition.Y, 3);
             Assert.Equal(0.004f, soulEater.WorldPosition.Z, 3);
+        }
+
+        [Fact]
+        public void LoadsTut01WorldPlacement()
+        {
+            Shade.ShadeCharmPlacementDatabase.Reload();
+            var placements = Shade.ShadeCharmPlacementDatabase.GetPlacementsForScene("Tut_01");
+
+            Assert.Single(placements);
+
+            var fury = placements.First();
+            Assert.Equal(Shade.ShadeCharmId.FuryOfTheFallen, fury.CharmId);
+            Assert.NotNull(fury.WorldPosition);
+            Assert.Equal(82.005f, fury.WorldPosition!.X, 3);
+            Assert.Equal(9.568f, fury.WorldPosition.Y, 3);
+            Assert.Equal(0.004f, fury.WorldPosition.Z, 3);
+        }
+
+        [Fact]
+        public void LoadsBonetownWorldPlacement()
+        {
+            Shade.ShadeCharmPlacementDatabase.Reload();
+            var placements = Shade.ShadeCharmPlacementDatabase.GetPlacementsForScene("Bonetown");
+
+            Assert.Single(placements);
+
+            var compass = placements.First();
+            Assert.Equal(Shade.ShadeCharmId.WaywardCompass, compass.CharmId);
+            Assert.NotNull(compass.WorldPosition);
+            Assert.Equal(301.342f, compass.WorldPosition!.X, 3);
+            Assert.Equal(24.568f, compass.WorldPosition.Y, 3);
+            Assert.Equal(0.004f, compass.WorldPosition.Z, 3);
         }
 
         [Fact]
